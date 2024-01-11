9619f42bf09e619a09f41c8fc9648ea68d44db9c
Zhou Chen En <109636871+patrick0516@users.noreply.github.com>
2024-01-11 21:07:12 +0800

    Create shorturl.py

---
+++ myhomework/mid/shorturl.py
@@ -0,0 +1,42 @@
+ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+def base62_encode(num, alphabet=ALPHABET):
+    """
+    10進制轉62進制
+    :param num:
+    :param alphabet:
+    :return:
+    """
+    if (num == 0):
+        return alphabet[0]
+    arr = []
+    base = len(alphabet)
+    while num:
+        rem = num % base
+        num = num // base
+        arr.append(alphabet[rem])
+    arr.reverse()
+    return ''.join(arr)
+
+def base62_decode(string, alphabet=ALPHABET):
+    """
+    62進制轉10進制
+    :param string:
+    :param alphabet:
+    :return:
+    """
+    base = len(alphabet)
+    strlen = len(string)
+    num = 0
+
+    idx = 0
+    for char in string:
+        power = (strlen - (idx + 1))
+        num += alphabet.index(char) * (base ** power)
+        idx += 1
+
+    return num
+
+if __name__ == '__main__':
+    print(base62_encode(4302842))
+    print(base62_decode('i3mG'))
